9271612956ad5c3bd788c2a29f39b67e095cca98
Justin Tan <justintws10@gmail.com>
2022-08-10 16:06:16 +1000

    finished MVP tic tac toe, attempt on connect 4

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@
 //2. Line them up in 3x3 line
 //3. If 'click' on a box, the box will change look. Idea - add source?
 
-//set up conditions for win
 
+
+//set up conditions for win
 const condition = [['a1', 'a2', 'a3'],      //0
                     ['b1', 'b2', 'b3'],     //1
                     ['c1', 'c2', 'c3'],     //2
@@ -17,73 +18,151 @@ const condition = [['a1', 'a2', 'a3'],      //0
                     ['a1', 'b2', 'c3'],
                     ['a3', 'b2', 'c1']]     //7
     
-
-const compareArray = function(playerArray) {
-    for (let i = 0; i < condition.length; i++) {
-        const winConditions = condition[i] //variable for win position loop
-        // console.log(winConditions)
-
-        let winCounter = 0
-        for (let j = 0; j < winConditions.length; j++) {   //looping through each element in each array
-            
-            if ( playerArray.includes( winConditions[j] ) === true ) {
-                winCounter ++
-
-                if ( winCounter === 3 ) {
-                    return true
-                }
-            }            
-        }// win condition for the each position in the triplet
-    } return false
-}
+//====================================================================
 
 
 //global variables
 let playerTurn = "One"
 let playerOne = [] //empty arrays to collect Ids as data for win conditions
 let playerTwo = []
+let playerOneWins = 0
+let playerTwoWins = 0
+
+$('#playerTurn').text(`Start with Player ${playerTurn} turn!`)
+
+
+//===================================================================
+
+
+//function for winning the game with player as argument
+const gameWin = function(playerArray) {
+    for (let i = 0; i < condition.length; i++) {
+
+        //variable for win position loop
+        const winConditions = condition[i] 
+        // console.log(winConditions)
+
+        let winCounter = 0
+        for (let j = 0; j < winConditions.length; j++) {   //looping through each element in each array
+            
+            if ( playerArray.includes( winConditions[j] ) === true ) {
+                winCounter ++ //if a value is detected as true in the one of the arrays in the condition, it adds +1 to the win counter without breaking the loop
+
+                if ( winCounter === 3 ) {
+                    return true
+                    //if win counter === 3, meaning all value is detected as true in the array because all arrays only have 3 values, and trigger the win
+                } 
+            }            
+        }// win condition for the each position in the triplet
+    } return false
+}
+
+
+//===================================================================
 
 
 const play = function () { 
 
-    //click handler
+    //click handler for any box
     $('.box').on('click', function() {
 
+        //if it is player one's turn
         if (playerTurn === "One") {
-            $(this).css('background-color', 'red')
-            $(this).css('pointer-events', 'none')
 
-            playerOne.push( $(this).attr('id') ); //obtain box ID and push into array
+            //change clicked box background, and make box unclickable after clicking
+            $(this).css('background', 'radial-gradient(#773535e0, #120101e1)')
+            $(this).css('pointer-events', 'none')
+            
+            //obtain box ID and push into player's array, announce other player's turn and make it other player's turn
+            playerOne.push( $(this).attr('id') ); 
             console.log(`playerOne's array ${playerOne}`)
             playerTurn = "Two"
+            $('#playerTurn').text(`It's Player ${playerTurn} turn!`)
 
-            if ( compareArray(playerOne) ) { //if playerOne wins
-                $('#frame').css('pointer-events', 'none')
-                console.log(`YAY`)
-            }
+            //if playerOne win, function with playerOne argument
+            if ( gameWin(playerOne) ) { 
+
+                //all boxes will become unclickable, playOne score will +1, the reset button and winning announcement will appear
+                $('.box').css('pointer-events', 'none')
+                playerOneWins ++
+                $('#pOneWinCount').text(`${playerOneWins}`)
+                $('#announcement').css('visibility', 'visible').text(`Player 1 wins!`)
+                $('#boardReset').css('visibility', 'visible')
+
+                //condition for best of 3
+                if (playerOneWins === 3) {
+                    $('#announcement').text(`Player One wins the grand connect 3 game!`)
+                    $('#gameReset').css('visibility', 'visible')
+                    $('#boardReset').css('visibility', 'hidden')
+                }
+                
+                //if it draws, announcement of draw will appear
+            } else if ( playerOne.length === 5 && gameWin(playerOne) !== true ) {
+                $('#announcement').css('visibility', 'visible').text(`It's a draw!`)
+                $('#boardReset').css('visibility', 'visible')
+
+            } 
             
+        //if it is player two's turn    
         } else if (playerTurn === "Two") {
-            $(this).css('background-color', 'green')
+            //change box background, and make box unclickable after clicking
+            $(this).css('background', 'radial-gradient( #265779dd, #010a18dd )')
             $(this).css('pointer-events', 'none')
             
+            //obtain box ID and push into player's array, announce other player's turn and make it other player's turn
             playerTwo.push( $(this).attr('id') );
             console.log(`playerTwo's array ${playerTwo}`)
             playerTurn = "One"
+            $('#playerTurn').text(`It's Player ${playerTurn} turn!`)
+            
+            //if playerTwo win, function with playerTwo argument
+            if ( gameWin(playerTwo) ) {
 
-            if ( compareArray(playerTwo) ) {
-                $('#frame').css('pointer-events', 'none')
-                console.log(`YAY`)
+                //all boxes will become unclickable, playOne score will +1, the reset button and winning announcement will appear
+                $('.box').css('pointer-events', 'none')
+                playerTwoWins ++
+                $('#pTwoWinCount').text(`${playerTwoWins}`)
+                $('#announcement').css('visibility', 'visible').text(`Player 2 wins!`)
+                $('#boardReset').css('visibility', 'visible')
+
+                //condition for best of 3
+                if (playerTwoWins === 3) {
+                    $('#announcement').text(`Player Two wins the grand connect 3 game!`)
+                    $('#gameReset').css('visibility', 'visible')
+                    $('#boardReset').css('visibility', 'hidden')
+                }
+                
+                //if it draws, announcement of draw will appear
+            } else if ( playerTwo.length === 5 && gameWin(playerTwo) !== true ) {
+                $('#announcement').css('visibility', 'visible').text(`It's a draw!`)
+                $('#boardReset').css('visibility', 'visible')
+                
             }
-    
         }
-    })
-    
-    
+    })  
 } // play function
 
 play()
 
+//===================================================================
 
+
+const resetBoard = function () {
+
+    //reset button to reset game board, internal js stuff ie arrays, reset clickability etc
+    $('#boardReset').on('click', function() {
+        $('.box').css( 'background', 'radial-gradient(#b6b3addd, #f1ebe4dd)' )
+        $('.box').css('pointer-events', 'auto')
+        $('#announcement').text('')
+        $('#boardReset').css('visibility', 'hidden')
+        playerOne = []
+        playerTwo = []
+    })  
+}
+
+resetBoard()
+
+//==================================================================
 
 //as a player click, get the ID box and push it to their player array
 
@@ -102,3 +181,25 @@ play()
 //8. make a reset button to reset the board
 
 
+
+
+//WEDNESDAY AGENDA
+//include details: <----PROBABLY DO THIS ONE FIRST
+//  a. give a button which will randomly select who goes first?
+//  b. set game set objective ie best of 3?
+//  c. announce who won the game clearly / game draw
+//  d. announce whose turn it is
+
+
+//set up css. make it presentable and pretty!
+//  a. have some animation ie fade?
+//  b. have a style / theme
+//  c. presentable fonts, proper layout
+
+//structure code:
+//  a. DRY it up 
+//  b. structure in a professional way? ie OOP.
+
+
+//THURSDAY AGENDA
+//ATTEMPT A BOT/ CONNECT FOUR
